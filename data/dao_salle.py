e4af955646066a9843e373ea61744705ca636c9d
Tarek Akli <300155086@monboreal.ca>
2026-04-19 18:08:18 -0400

    ajout de la methode get_connection

--- data/dao_salle.py
+++ data/dao_salle.py
@@ -3,3 +3,8 @@ import mysql.connector
 from models.salle import Salle<
 
 class DataSalle:
+    def __init__(self):
+        pass
+    def get_connection(self):
+        with open("data/config.json", "r") as f:
+            config = json.load(f)
