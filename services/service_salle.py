d574af950a5c5fb7acf44209a829e2484d36503c
Tarek Akli <300155086@monboreal.ca>
2026-04-19 18:46:24 -0400

    ajout de la methode ajouter_salle

--- services/service_salle.py
+++ services/service_salle.py
@@ -4,3 +4,14 @@ from data.dao_salle import DataSalle
 class ServiceSalle:
     def __init__(self):
         self.dao_salle = DataSalle()
+
+    def ajouter_salle(self, salle):
+        if not salle.code or not salle.description or not salle.categorie or salle.capacite is None:
+            return False, "Tous les champs sont obligatoires"
+
+        if int(salle.capacite) < 1:
+            return False, "La capacite doit etre superieure ou egale a 1"
+
+        self.dao_salle.insert_salle(salle)
+        return True, "Salle ajoutee avec succes"
+
