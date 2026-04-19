ced2083c26183b609415613059d5b4fd0e6cf11d
Tarek Akli <300155086@monboreal.ca>
2026-04-19 18:51:59 -0400

    ajout de la methode modifier_salle(salle)

--- services/service_salle.py
+++ services/service_salle.py
@@ -15,3 +15,13 @@ class ServiceSalle:
         self.dao_salle.insert_salle(salle)
         return True, "Salle ajoutee avec succes"
 
+    def modifier_salle(self, salle):
+        if not salle.code or not salle.description or not salle.categorie or salle.capacite is None:
+            return False, "Tous les champs sont obligatoires"
+
+        if int(salle.capacite) < 1:
+            return False, "La capacite doit etre superieure ou egale a 1"
+
+        self.dao_salle.update_salle(salle)
+        return True, "Salle modifiee avec succes"
+
