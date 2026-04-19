9a4487802d639b4c13395010a9631cfb98dd006c
Tarek Akli <300155086@monboreal.ca>
2026-04-19 17:53:59 -0400

    definir les attributs de la classe salle

--- views/view_salle.py
+++ views/view_salle.py
@@ -1,2 +1,8 @@
 class Salle:
     def __init__(self, code, description, categorie, capacite):
+        self.code = code
+        self.description = description
+        self.categorie = categorie
+        self.capacite = capacite
+
+
