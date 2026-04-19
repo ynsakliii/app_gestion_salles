30d264695a43406da09c99868890bd6db2fa9444
Tarek Akli <300155086@monboreal.ca>
2026-04-19 18:33:34 -0400

    ajout de la methode get_salles()

--- data/dao_salle.py
+++ data/dao_salle.py
@@ -83,3 +83,20 @@ def get_salle(self, code):
         return Salle(row[0], row[1], row[2], row[3])
     return None
 
+
+def get_salles(self):
+    conn = self.get_connection()
+    cursor = conn.cursor()
+
+    query = "SELECT code, description, categorie, capacite FROM salle"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    salles = []
+    for row in rows:
+        salles.append(Salle(row[0], row[1], row[2], row[3]))
+
+    return salles
