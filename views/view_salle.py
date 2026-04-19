16dd6f908c6da47e11ac62e630ff20340b18a1ad
Tarek Akli <300155086@monboreal.ca>
2026-04-19 19:07:25 -0400

    ajout de la classe ViewSalle

--- views/view_salle.py
+++ views/view_salle.py
@@ -1,1 +1,17 @@
+import customtkinter as ctk
+from tkinter import ttk, messagebox
 
+from models.salle import Salle
+from services.service_salle import ServiceSalle
+
+
+class ViewSalle(ctk.CTK):
+    def __init__(self):
+        super().__init__()
+        self.title("Gestion des salles")
+        self.geometry("750x500")
+
+        self.service_salle = ServiceSalle()
+
+        self.creer_widgets()
+        self.lister_salles()
